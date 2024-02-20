b1d9a5a7f1803b650b6a24a74d0cdf74c82d0006
Joyyx <106960377+Joyyx@users.noreply.github.com>
2024-02-20 14:16:10 +0700

    main.py

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def penambahan(a,b):
 def pengurangan(a,b):
 	total = a - b
 	return total
-
+def perkalian(a,b):
+	total = a * b
+	return perkalian
 def main():
 	print(penambahan(10,5))
 	print(pengurangan(10,5))
+	print(perkalian(10,5))
 main()
