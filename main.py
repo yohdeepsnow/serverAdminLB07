291d464fc8b9f500343bd91d53775d0940aa880f
yohdeepsnow <yohan.muliono@gmail.com>
2024-02-20 06:45:54 +0000

    pembuatan kerangka program dengan fungsi penambahan

---
+++ main.py
@@ -0,0 +1,11 @@
+#tolong buat fungsi lainnya untuk pengurangan, perkalian, pembagian
+#dengan format yang sama
+
+def penambahan(a,b):
+	total = a + b
+	return total
+
+def main():
+	print(penambahan(10,5))
+
+main()
